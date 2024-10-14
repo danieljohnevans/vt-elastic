03d6e49392a36365ea07d67b5aa7de1a54342b94
danieljohnevans <djevan11@gmail.com>
2024-10-14 17:15:52 -0400

    minor wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,12 @@
 import sys
 import os
 from dotenv import load_dotenv
+
+path = '/var/www/webroot/ROOT'
+if path not in sys.path:
+    sys.path.append(path)
+
+    
 from app import app  
 
 load_dotenv()
@@ -12,8 +18,5 @@ password = os.getenv("ES_PASSWORD")
 if not username or not password:
     raise RuntimeError("Elasticsearch credentials (ES_USER, ES_PASSWORD) are not set in the environment variables.")
 
-path = '/var/www/webroot/ROOT'
-if path not in sys.path:
-    sys.path.append(path)
 
 application = app
