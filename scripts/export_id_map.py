56733a6fb494ff689d4fbdef1c7b65eeb908d715
danieljohnevans <djevan11@gmail.com>
2026-03-24 10:39:24 -0400

    fixing memory issue

--- scripts/export_id_map.py
+++ scripts/export_id_map.py
@@ -21,28 +21,26 @@ from elasticsearch.helpers import scan
 from vt.elastic import elastic_client
 
 es = elastic_client()
-id_map = {}
+
+out_path = os.path.join(os.path.dirname(__file__), "uid_to_esid.jsonl")
 
 print("Scrolling through viral-texts index...")
-for hit in scan(es, index="viral-texts", _source=["uid", "begin"], scroll="30m"):
-    uid = str(hit["_source"]["uid"])
-    begin = str(hit["_source"].get("begin", ""))
-    key = f"{uid}:{begin}"
-    id_map[key] = hit["_id"]
-
-    # Also add float64-truncated uid variant for Globus data compatibility
-    try:
-        uid_int = int(uid)
-        truncated = str(int(Decimal(repr(float(uid_int)))))
-        if truncated != uid:
-            trunc_key = f"{truncated}:{begin}"
-            if trunc_key not in id_map:
-                id_map[trunc_key] = hit["_id"]
-    except (ValueError, OverflowError):
-        pass
-
-out_path = os.path.join(os.path.dirname(__file__), "uid_to_esid.json")
 with open(out_path, "w") as f:
-    json.dump(id_map, f)
+    for hit in scan(es, index="viral-texts", _source=["uid", "begin"], scroll="30m"):
+        uid = str(hit["_source"]["uid"])
+        begin = str(hit["_source"].get("begin", ""))
+        key = f"{uid}:{begin}"
 
-print(f"Exported {len(id_map)} uid:begin -> _id mappings to {out_path}")
+        f.write(json.dumps({key: hit["_id"]}) + "\n")
+
+        # Also add float64-truncated uid variant
+        try:
+            uid_int = int(uid)
+            truncated = str(int(Decimal(repr(float(uid_int)))))
+            if truncated != uid:
+                trunc_key = f"{truncated}:{begin}"
+                f.write(json.dumps({trunc_key: hit["_id"]}) + "\n")
+        except (ValueError, OverflowError):
+            pass
+
+print(f"Export complete -> {out_path}")
